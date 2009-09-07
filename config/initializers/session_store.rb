7eca3d30435a78abc67b20c0dc3b09b9e682e1ee
Michelangelo Altamore <michelangelo@altamore.org>
2009-09-07 11:45:13 +0200

    initial import

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_bios_invoice_manager_session',
+  :secret      => '3726fad8e3256e06dfd6f3a33bd23b22c9b11730bb7a4e937e6eb549fa80ee3ed9a058cbd615f5a3c9572c51d1eb9c2904e4dcc036f7671df4700e5c8d549d32'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
