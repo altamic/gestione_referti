b938e3bd7a06c06bb1f9a101c8b6923879eadcb6
Michelangelo Altamore <michelangelo@altamore.org>
2009-09-07 19:30:37 +0200

    fixing criteria

--- app/models/invoice_trail.rb
+++ app/models/invoice_trail.rb
@@ -7,7 +7,22 @@ class InvoiceTrail < ActiveRecord::Base
   
   # delegate 
   
+  named_scope :payment_completed, :conditions => ['payed = ?', true]
+  named_scope :payment_uncompleted, :conditions => ['payed = ?', false]
+  
+  named_scope :payment_status, lambda { |value| self.send(:compute_conditions, value) }
+  
+  def payment_status
+    payed ? "payment_completed" : "payment_uncompleted"
+  end
+    
   def full_name
     [last_name, first_name].join(' ')
+  end
+
+
+  private
+  def self.compute_conditions(value)
+    self.send(value).proxy_options
   end
 end
