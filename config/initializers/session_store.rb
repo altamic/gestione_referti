368210df8ca824592b16fe287fb2f48cc8844598
Michelangelo Altamore <michelangelo@altamore.org>
2009-10-19 23:09:06 +0200

    renamed app to gestione referti

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_bios_invoice_manager_session',
-  :secret      => '3726fad8e3256e06dfd6f3a33bd23b22c9b11730bb7a4e937e6eb549fa80ee3ed9a058cbd615f5a3c9572c51d1eb9c2904e4dcc036f7671df4700e5c8d549d32'
+  :key         => '_gestione_referti_session',
+  :secret      => '3726fa7e6eb549fa80ee3e6e06dfd6f3a33bd239a058cbd615f5a3c9d8e32b22c9b11730bb7a4e9372c5d51d1eb9c2904e4dcc036f7671df4700e5c8d549d32'
 }
 
 # Use the database for sessions instead of the cookie-based default,
