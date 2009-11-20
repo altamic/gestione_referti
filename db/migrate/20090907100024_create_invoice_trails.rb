e94140dbc55a9538d637913d8f9330b233f55fff
Michelangelo Altamore <michelangelo@altamore.org>
2009-11-20 18:47:03 +0100

    d

--- db/migrate/20090907100024_create_invoice_trails.rb
+++ db/migrate/20090907100024_create_invoice_trails.rb
@@ -4,9 +4,9 @@ class CreateInvoiceTrails < ActiveRecord::Migration
       t.date :admission_date
       t.string :admission_code
       t.string :full_name
-      t.decimal :gross_amount
-      t.decimal :discounted_amount
-      t.decimal :discount
+      t.decimal :gross_amount     , :precision => 5, :scale => 2, :default => 40.0
+      t.decimal :discounted_amount, :precision => 5, :scale => 2, :default => 0.0
+      t.decimal :discount         , :precision => 5, :scale => 2, :default => 0.0
       t.boolean :payed, :default => false
       t.date :payment_date
       t.integer :invoice_number
