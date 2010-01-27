4ff51ea5ee9b339f77613133a3a64b58c6f2a932
Michelangelo Altamore <michelangelo@altamore.org>
2010-01-27 13:57:30 +0100

    error message fixed

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.gem "calendar_date_select", :version => '~> 1.15'
   config.gem "searchlogic", :version => '~> 2.3.5'
-  config.gem "will_paginate", :version  => '~> 2.2.2'
+  config.gem "will_paginate", :version  => '~> 2.3.0'
   config.gem "calendar_date_select", :version  => '1.15'
   
   config.time_zone = 'Rome'
